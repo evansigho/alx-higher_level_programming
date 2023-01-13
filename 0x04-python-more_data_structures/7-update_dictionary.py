4860870330ff50ab5ae23eef787ac01a195cd1f1
evansigho <evansigho4@gmail.com>
2023-01-13 12:03:06 -0800

    new commit

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(my_dict):
-    for key in sorted(my_dict.keys()):
-        print("{:s}: {}".format(key, my_dict[key]))
+def update_dictionary(my_dict, key, value):
+    my_dict[key] = value
+    return my_dict
